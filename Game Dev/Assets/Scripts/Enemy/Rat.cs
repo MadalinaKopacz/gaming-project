96f98afccccf532cc382d08f0a26ab09f81a4cf2
AndreeaG <andreea.g2001@yahoo.com>
2022-11-19 17:44:54 +0200

    Fix Rat Animation

--- Game Dev/Assets/Scripts/Enemy/Rat.cs
+++ Game Dev/Assets/Scripts/Enemy/Rat.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Rat : MonoBehaviour
 {
     [SerializeField] private int hp = 10;
+    [SerializeField] private Transform player;
+    private Rigidbody2D rb;
 
     //to be implemented in the next sprint
     //[SerializeField] private int damage = 20;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
@@ -19,6 +26,14 @@ public class Rat : MonoBehaviour
             if (hp <= 0)
             {
                 Destroy(gameObject);
+            }
+        }
+
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            if(transform.position.y <= player.position.y)
+            {
+                rb.velocity = Vector3.zero;
             }
         }
     }
